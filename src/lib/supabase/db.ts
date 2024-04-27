891738101d0ab20f0237658cf7aa256ad4bf0d2d
Lone Wolf <pr099rimalpiyush@gmail.com>
2024-04-27 12:35:43 +0545

    created db and schema for drizzle and postgres'

---
+++ src/lib/supabase/db.ts
@@ -0,0 +1,14 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as dotenv from 'dotenv';
+import * as schema from '../../../migrations/schema';
+dotenv.config({ path: '.env' });
+
+if (!process.env.DATABASE_URL) {
+    console.log('* No database url');
+}
+
+const client = postgres(process.env.DATABASE_URL, { max: 1 });
+const db = drizzle(client, { schema });
+
+export default db;
