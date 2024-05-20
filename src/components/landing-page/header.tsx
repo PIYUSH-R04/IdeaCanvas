a826b90b9729750fc34632a0aba13030c39bc2b0
Lone Wolf <pr099rimalpiyush@gmail.com>
2024-05-20 23:59:31 +0545

    working on navbar

--- src/components/landing-page/header.tsx
+++ src/components/landing-page/header.tsx
@@ -4,17 +4,17 @@ import Link from 'next/link';
 import React, { useState } from 'react';
 import Logo from '../../../public/client1.png';
 
-// import {
-//   NavigationMenu,
-//   NavigationMenuContent,
-//   NavigationMenuIndicator,
-//   NavigationMenuItem,
-//   NavigationMenuLink,
-//   NavigationMenuList,
-//   NavigationMenuTrigger,
-//   NavigationMenuViewport,
-//   navigationMenuTriggerStyle,
-// } from '@/components/ui/navigation-menu';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
 import { cn } from '@/lib/utils';
 import { Button } from '../ui/Button';
 
@@ -92,7 +92,158 @@ const Header = () => {
           IdeaCanvas.
         </span>
       </Link>
-      
+      <NavigationMenu className="hidden md:block">
+        <NavigationMenuList className="gap-6">
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              onClick={() => setPath('#resources')}
+              className={cn({
+                'dark:text-white': path === '#resources',
+                'dark:text-white/40': path !== '#resources',
+                'font-normal': true,
+                'text-xl': true,
+              })}
+            >
+              Resources
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul
+                className="grid
+                gap-3
+                p-6
+                md:w-[400px]
+                ld:w-[500px]
+                lg:grid-cols-[.75fr_1fr]
+                "
+              >
+                <li className="row-span-3">
+                  <span
+                    className="flex h-full w-full select-none
+                  flex-col
+                  justify-end
+                  rounded-md
+                  bg-gradient-to-b
+                  from-muted/50
+                  to-muted
+                  p-6 no-underline
+                  outline-none
+                  focus:shadow-md
+                  "
+                  >
+                    Welcome
+                  </span>
+                </li>
+                <ListItem
+                  href="#"
+                  title="Introduction"
+                >
+                  Re-usable components built using Radix UI and Tailwind CSS.
+                </ListItem>
+                <ListItem
+                  href="#"
+                  title="Installation"
+                >
+                  How to install dependencies and structure your app.
+                </ListItem>
+                <ListItem
+                  href="#"
+                  title="Typography"
+                >
+                  Styles for headings, paragraphs, lists...etc
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger
+              onClick={() => setPath('#pricing')}
+              className={cn({
+                'dark:text-white': path === '#pricing',
+                'dark:text-white/40': path !== '#pricing',
+                'font-normal': true,
+                'text-xl': true,
+              })}
+            >
+              Pricing
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4  md:grid-row-2  ">
+                <ListItem
+                  title="Pro Plan"
+                  href={'#'}
+                >
+                  Unlock full power with collaboration.
+                </ListItem>
+                <ListItem
+                  title={'free Plan'}
+                  href={'#'}
+                >
+                  Great for teams just starting out.
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuContent>
+              <ul
+                className="grid w-[400px]
+              gap-3
+              p-4
+              md:w-[500px]
+              md:grid-cols-2 
+              lg:w-[600px]
+              "
+              >
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    href={component.href}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className={cn(navigationMenuTriggerStyle(), {
+                'dark:text-white': path === '#testimonials',
+                'dark:text-white/40': path !== '#testimonials',
+                'font-normal': true,
+                'text-xl': true,
+              })}
+            >
+              Testimonial
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <aside
+        className="flex
+        w-full
+        gap-2
+        justify-end
+      "
+      >
+        <Link href={'/login'}>
+          <Button
+            variant="btn-secondary"
+            className=" p-1 hidden sm:block"
+          >
+            Login
+          </Button>
+        </Link>
+        <Link href="/signup">
+          <Button
+            variant="outline"
+            className="whitespace-nowrap"
+          >
+            Sign Up
+          </Button>
+        </Link>
+      </aside>
     </header>
   );
 };
@@ -105,7 +256,7 @@ const ListItem = React.forwardRef<
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
-      {/* <NavigationMenuLink asChild>
+      {<NavigationMenuLink asChild>
         <a
           ref={ref}
           className={cn(
@@ -127,7 +278,7 @@ const ListItem = React.forwardRef<
             {children}
           </p>
         </a>
-      </NavigationMenuLink> */}
+      </NavigationMenuLink>}
     </li>
   );
 });
@@ -141,155 +292,3 @@ ListItem.displayName = 'ListItem';
 
 
 
-// <NavigationMenu className="hidden md:block">
-//         <NavigationMenuList className="gap-6">
-//           <NavigationMenuItem>
-//             <NavigationMenuTrigger
-//               onClick={() => setPath('#resources')}
-//               className={cn({
-//                 'dark:text-white': path === '#resources',
-//                 'dark:text-white/40': path !== '#resources',
-//                 'font-normal': true,
-//                 'text-xl': true,
-//               })}
-//             >
-//               Resources
-//             </NavigationMenuTrigger>
-//             <NavigationMenuContent>
-//               <ul
-//                 className="grid
-//                 gap-3
-//                 p-6
-//                 md:w-[400px]
-//                 ld:w-[500px]
-//                 lg:grid-cols-[.75fr_1fr]
-//                 "
-//               >
-//                 <li className="row-span-3">
-//                   <span
-//                     className="flex h-full w-full select-none
-//                   flex-col
-//                   justify-end
-//                   rounded-md
-//                   bg-gradient-to-b
-//                   from-muted/50
-//                   to-muted
-//                   p-6 no-underline
-//                   outline-none
-//                   focus:shadow-md
-//                   "
-//                   >
-//                     Welcome
-//                   </span>
-//                 </li>
-//                 <ListItem
-//                   href="#"
-//                   title="Introduction"
-//                 >
-//                   Re-usable components built using Radix UI and Tailwind CSS.
-//                 </ListItem>
-//                 <ListItem
-//                   href="#"
-//                   title="Installation"
-//                 >
-//                   How to install dependencies and structure your app.
-//                 </ListItem>
-//                 <ListItem
-//                   href="#"
-//                   title="Typography"
-//                 >
-//                   Styles for headings, paragraphs, lists...etc
-//                 </ListItem>
-//               </ul>
-//             </NavigationMenuContent>
-//           </NavigationMenuItem>
-//           <NavigationMenuItem>
-//             <NavigationMenuTrigger
-//               onClick={() => setPath('#pricing')}
-//               className={cn({
-//                 'dark:text-white': path === '#pricing',
-//                 'dark:text-white/40': path !== '#pricing',
-//                 'font-normal': true,
-//                 'text-xl': true,
-//               })}
-//             >
-//               Pricing
-//             </NavigationMenuTrigger>
-//             <NavigationMenuContent>
-//               <ul className="grid w-[400px] gap-3 p-4  md:grid-row-2  ">
-//                 <ListItem
-//                   title="Pro Plan"
-//                   href={'#'}
-//                 >
-//                   Unlock full power with collaboration.
-//                 </ListItem>
-//                 <ListItem
-//                   title={'free Plan'}
-//                   href={'#'}
-//                 >
-//                   Great for teams just starting out.
-//                 </ListItem>
-//               </ul>
-//             </NavigationMenuContent>
-//           </NavigationMenuItem>
-//           <NavigationMenuItem>
-//             <NavigationMenuContent>
-//               <ul
-//                 className="grid w-[400px]
-//               gap-3
-//               p-4
-//               md:w-[500px]
-//               md:grid-cols-2 
-//               lg:w-[600px]
-//               "
-//               >
-//                 {components.map((component) => (
-//                   <ListItem
-//                     key={component.title}
-//                     title={component.title}
-//                     href={component.href}
-//                   >
-//                     {component.description}
-//                   </ListItem>
-//                 ))}
-//               </ul>
-//             </NavigationMenuContent>
-//           </NavigationMenuItem>
-//           <NavigationMenuItem>
-//             <NavigationMenuLink
-//               className={cn(navigationMenuTriggerStyle(), {
-//                 'dark:text-white': path === '#testimonials',
-//                 'dark:text-white/40': path !== '#testimonials',
-//                 'font-normal': true,
-//                 'text-xl': true,
-//               })}
-//             >
-//               Testimonial
-//             </NavigationMenuLink>
-//           </NavigationMenuItem>
-//         </NavigationMenuList>
-//       </NavigationMenu>
-//       <aside
-//         className="flex
-//         w-full
-//         gap-2
-//         justify-end
-//       "
-//       >
-//         <Link href={'/login'}>
-//           <Button
-//             variant="btn-secondary"
-//             className=" p-1 hidden sm:block"
-//           >
-//             Login
-//           </Button>
-//         </Link>
-//         <Link href="/signup">
-//           <Button
-//             variant="btn-primary"
-//             className="whitespace-nowrap"
-//           >
-//             Sign Up
-//           </Button>
-//         </Link>
-//       </aside>
